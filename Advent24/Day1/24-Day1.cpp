f0a7c4ad183d6b79479b8ec1714682d3cccb3904
Magnus <magnuslindland@protonmail.ch>
2024-12-01 11:33:19 -0700

    Solved part two

--- Advent24/Day1/24-Day1.cpp
+++ Advent24/Day1/24-Day1.cpp
@@ -8,6 +8,7 @@
 int main()
 {
     int total = 0;
+    int similarity = 0;
     std::ifstream file;
     std::string line;
     std::vector<int> row_one;
@@ -42,6 +43,23 @@ int main()
             }
         }
     }
+
+    for (auto i = 0; i < row_one.size(); i++)
+    {
+        int iteration_count = 0;
+        int iteration_numner = row_one[i];
+        
+        for (auto j = 0; j < row_two.size(); j++)
+        {
+            if (iteration_numner == row_two[j])
+            {
+                iteration_count++;
+            }
+        }
+        similarity += iteration_count * iteration_numner;            
+    }
+    std::cout << "Total Similarity: " << similarity << std::endl;
+
     std::sort(row_one.begin(), row_one.end());
     std::sort(row_two.begin(), row_two.end());
     
